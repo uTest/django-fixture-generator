67aeb4ae3498b73c21bfd94bd82056a916620310
Piotr Duda <pduda@applause.com>
2016-08-09 15:05:53 +0200

    Django 1.9 Support

--- fixture_generator/base.py
+++ fixture_generator/base.py
@@ -1,6 +1,7 @@
-from django.utils.importlib import import_module
 from django.utils.module_loading import module_has_submodule
 from collections import namedtuple
+from importlib import import_module
+
 
 
 class Fixture(namedtuple("Fixture", "app name export func")):
